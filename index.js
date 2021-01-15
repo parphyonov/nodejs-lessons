015462c59a22afac45e43aa96affce2bd0b0ca17
Alexei Parphyonov <alexeiparphyonov@gmail.com>
2021-01-16 00:48:25 +0300

    made some notes in index.js

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const http = require('http');
 const jokes = require('jokes');
 
 console.log(jokes());
+
+// Start your app with NODE_ENV=myEnvironmentName node index.js
+// Put your configuration in a file (e.g. config.js) which has a switch inside of it.
+// That switch should read process.env.NODE_ENV to determine the current environment and export only the config variables for that environment.
+
+// https://github.com/airbnb/javascript
